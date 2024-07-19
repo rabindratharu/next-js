f947c68aa72602c954be50245fe8b8f096715e4c
rabindra tharu <rabindra.tharu.np@gmail.com>
2024-07-19 17:20:30 +0545

    fixes

--- src/components/layout/header/index.js
+++ src/components/layout/header/index.js
@@ -1,29 +1,29 @@
 import { isEmpty } from "lodash";
-import Link from "next/link";
-const Header = ({ headerMenus }) => {
+import PropTypes from "prop-types";
+
+import Nav from "./nav";
+const Header = ({ header, headerMenus, slug }) => {
   if (isEmpty(headerMenus)) {
     return null;
   }
+
   return (
-    <>
-      <header>
-        <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
-          {headerMenus?.length ? (
-            <div>
-              {headerMenus?.map((menu) => {
-                return (
-                  <Link key={menu?.node.id} href={menu?.node?.path}>
-                    {menu?.node?.label}
-                  </Link>
-                );
-              })}
-            </div>
-          ) : null}
-        </nav>
-        Header
-      </header>
-    </>
+    <header>
+      <Nav header={header} headerMenus={headerMenus} slug={slug} />
+    </header>
   );
+};
+
+Header.propTypes = {
+  header: PropTypes.object,
+  headerMenus: PropTypes.array,
+  slug: PropTypes.string,
+};
+
+Header.defaultProps = {
+  header: {},
+  headerMenus: [],
+  slug: "",
 };
 
 export default Header;
