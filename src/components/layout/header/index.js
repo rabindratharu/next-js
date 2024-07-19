895c2fd849765b3ce56bd93a37f81e70ee65c398
rabindra tharu <rabindra.tharu.np@gmail.com>
2024-07-19 17:44:37 +0545

    fixes

--- src/components/layout/header/index.js
+++ src/components/layout/header/index.js
@@ -2,28 +2,16 @@ import { isEmpty } from "lodash";
 import PropTypes from "prop-types";
 
 import Nav from "./nav";
-const Header = ({ header, headerMenus, slug }) => {
+const Header = ({ headerMenus }) => {
   if (isEmpty(headerMenus)) {
     return null;
   }
 
   return (
     <header>
-      <Nav header={header} headerMenus={headerMenus} slug={slug} />
+      <Nav headerMenus={headerMenus} />
     </header>
   );
-};
-
-Header.propTypes = {
-  header: PropTypes.object,
-  headerMenus: PropTypes.array,
-  slug: PropTypes.string,
-};
-
-Header.defaultProps = {
-  header: {},
-  headerMenus: [],
-  slug: "",
 };
 
 export default Header;
--- src/components/layout/header/nav.js
+++ src/components/layout/header/nav.js
@@ -1,42 +1,34 @@
 import { isEmpty } from "lodash";
 import Link from "next/link";
 import PropTypes from "prop-types";
+
 import { isCustomPageUri } from "../../../utils/slug";
 
-const Nav = ({ header, headerMenus, slug }) => {
+const Nav = ({ headerMenus }) => {
   if (isEmpty(headerMenus)) {
     return null;
   }
 
+  console.log(headerMenus);
+
   return (
     <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
-      {headerMenus?.length ? (
-        <div className="text-sm lg:flex-grow">
-          {headerMenus?.map((menu) => {
-            if (!isCustomPageUri(menu?.node?.path)) {
-              return (
-                <Link key={menu?.node.id} href={menu?.node?.path}>
-                  {menu?.node?.label}
-                </Link>
-              );
-            }
-          })}
-        </div>
-      ) : null}
+      <div>
+        {headerMenus?.length ? (
+          <div className="text-sm lg:flex-grow">
+            {headerMenus?.map((menu) => {
+              if (!isCustomPageUri(menu?.node?.path)) {
+                return (
+                  <Link key={menu?.node.id} href={menu?.node?.path}>
+                    {menu?.node?.label}
+                  </Link>
+                );
+              }
+            })}
+          </div>
+        ) : null}
+      </div>
     </nav>
   );
 };
-
-Nav.propTypes = {
-  header: PropTypes.object,
-  headerMenus: PropTypes.array,
-  slug: PropTypes.string,
-};
-
-Nav.defaultProps = {
-  header: {},
-  headerMenus: [],
-  slug: "",
-};
-
 export default Nav;
